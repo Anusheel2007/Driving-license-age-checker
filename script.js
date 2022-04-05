e4707a32624a2a19f5cf6447b8d7fc1f667f8c40
Anusheel2007 <102905713+Anusheel2007@users.noreply.github.com>
2022-04-05 19:22:44 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+const minimumAge = 18;
+let currentAge = 17 ;
+if (currentAge >= 18) {
+    console.log('You are eligible for Driving license ');
+} 
+else if (currentAge >= 16) {
+    console.log("You're eligible for Learning Driving license")
+}
+else {
+    const timeYouNeed = minimumAge-currentAge;
+    console.log(`You are not eligible for Driving license, you are ${timeYouNeed} younger then the minimum age`) }
+const minimumAge = 18;
+let currentAge = 17 ;
+if (currentAge >= 18) {
+    console.log('You are eligible for Driving license ');
+} 
+else if (currentAge >= 16) {
+    console.log("You're eligible for Learning Driving license")
+}
+else {
+    const timeYouNeed = minimumAge-currentAge;
+    console.log(`You are not eligible for Driving license, you are ${timeYouNeed} younger then the minimum age`) }
